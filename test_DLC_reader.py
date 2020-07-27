e5f9c076f57cb5404ff52416a2b7b819c5791ee6
zerotonin <bgeurte@gwdg.de>
2020-07-27 13:09:27 +0200

    add plotting

--- test_DLC_reader.py
+++ test_DLC_reader.py
@@ -1,5 +1,6 @@
-import DLC_reader 
+import DLC_reader,tqdm 
 from importlib import reload 
+import matplotlib.pyplot as plt
 
 
 reload(DLC_reader)
@@ -7,3 +8,24 @@ fPos = '/media/dataSSD/Anka4/2019-09-25__10_23_32DeepCut_resnet50_darkClimbFlies
 x = DLC_reader.DLC_H5_reader(fPos)  
 x.readH5()
 x.multiAnimal2numpy()
+
+
+#fig, ax = plt.subplots()
+#ax.hold(True)
+cmap = plt.get_cmap('tab20')      
+for frame in tqdm.tqdm(x.tra):
+    rows,cols = frame.shape
+    c = 0
+    for rowI in range(rows):
+        if rowI%2 == 0:
+            plt.plot(frame[rowI,0],frame[rowI,1],'x',color=cmap.colors[c])
+        else:
+            plt.plot(frame[rowI,0],frame[rowI,1],'o',color=cmap.colors[c])
+            c+=1
+plt.hold(False)
+
+plt.show()
+
+
+    
+    
