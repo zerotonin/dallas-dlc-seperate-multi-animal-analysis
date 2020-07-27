75f1a549f31cbbb3226bc213c47239d67ce11942
zerotonin <bgeurte@gwdg.de>
2020-07-27 11:00:39 +0200

    reader update

--- DLC_reader.py
+++ DLC_reader.py
@@ -1,4 +1,5 @@
 import pandas,tqdm
+import numpy as np
 
 class DLC_H5_reader:
     def __init__(self,filePosition):
@@ -11,13 +12,18 @@ class DLC_H5_reader:
     
     def readH5(self):
         self.pandasDF = pandas.read_hdf(self.fPos)
-        self.frameNo  = self.pandasDF.pandasDF.shape[0]  
-        self.columns  = self.pandasDF.pandasDF.shape[1]
-        self.areaNo   = self.columns/4  
+        self.frameNo  = self.pandasDF.shape[0]  
+        self.columns  = self.pandasDF.shape[1]
+        self.areaNo   = int(self.columns/3)  
     
     def multiAnimal2numpy(self):
-        for frameI in tqdm.tqdm(range(df_tra.shape[0])):
-            frameRes = self.pandasDF.iloc[frameI].iloc[:]  
+        self.tra = list()
+        for frameI in tqdm.tqdm(range(self.frameNo)):
+            frameRes = self.pandasDF.iloc[frameI].iloc[:] 
+            frameRes = frameRes.to_numpy()  
+            frameRes = np.reshape(frameRes,(self.areaNo,-1))  
+            self.tra.append(frameRes)
+           
             
 
 
--- test_DLC_reader.py
+++ test_DLC_reader.py
@@ -5,3 +5,5 @@ from importlib import reload
 reload(DLC_reader)
 fPos = '/media/dataSSD/Anka4/2019-09-25__10_23_32DeepCut_resnet50_darkClimbFliesSep27shuffle1_1030000.h5'
 x = DLC_reader.DLC_H5_reader(fPos)  
+x.readH5()
+x.multiAnimal2numpy()
