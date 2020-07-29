c386ea97e60e2e4f683828624df64c80db18da15
zerotonin <bgeurte@gwdg.de>
2020-07-29 15:41:10 +0200

    update

--- test_DLC_reader.py
+++ test_DLC_reader.py
@@ -6,6 +6,8 @@ import numpy as np
 
 reload(DLC_reader)
 flyPos    = '/media/dataSSD/Anka1/A_01_01DeepCut_resnet50_ParalellClimb2Aug22shuffle1_150000.h5'
+flyPos    = '/media/dataSSD/Anka4/2019-09-25__10_18_09DeepCut_resnet50_darkClimbFliesSep27shuffle1_1030000.h5'
+
 cornerPos2= '/media/dataSSD/Anka1/A_01_01DeepCut_resnet50_darkClimbCornersSep13shuffle1_1030000.h5'
 
 x = DLC_reader.DLC_H5_reader(flyPos,15)  
@@ -26,17 +28,17 @@ ax.set_ylim(0,800)
 ax.axis('equal')
 plt.gca().invert_yaxis()
 
-for frameI in np.linspace(0,x.frameNo,6002, endpoint=False, dtype=int ):
+for frameI in np.linspace(0,x.frameNo,100, endpoint=False, dtype=int ):
     
-    #for animalI in  range(x.animalNo):
-    animalI =5
-    if y.artifactCandidates[frameI,animalI]:
-        ax.plot(y.tra[frameI,animalI,:,0],y.tra[frameI,animalI,:,1],'k-')        
-        ax.plot(y.tra[frameI,animalI,:,0],y.tra[frameI,animalI,:,1],'kx')
-    else:
-        ax.plot(y.tra[frameI,animalI,:,0],y.tra[frameI,animalI,:,1],'-',color=cmap.colors[animalI])        
-        ax.plot(y.tra[frameI,animalI,0,0],y.tra[frameI,animalI,0,1],'.',color=cmap.colors[animalI])
-    
-    #fig.canvas.draw()
-plt.show()
+    for animalI in  range(x.animalNo):
+    #animalI =5
+        if y.artifactCandidates[frameI,animalI]:
+            ax.plot(y.tra[frameI,animalI,:,0],y.tra[frameI,animalI,:,1],'k-')        
+            ax.plot(y.tra[frameI,animalI,:,0],y.tra[frameI,animalI,:,1],'kx')
+        else:
+            ax.plot(y.tra[frameI,animalI,:,0],y.tra[frameI,animalI,:,1],'-',color=cmap.colors[animalI])        
+            ax.plot(y.tra[frameI,animalI,0,0],y.tra[frameI,animalI,0,1],'.',color=cmap.colors[animalI])
+        
+        fig.canvas.draw()
+#plt.show()
 
