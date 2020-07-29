a33f421fc74a595e6a5c3c38d35ab63d0d641f57
zerotonin <bgeurte@gwdg.de>
2020-07-29 10:38:22 +0200

    first functions for index manipulations

--- indexTools.py
+++ indexTools.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-class indexTools:
-    def __init__(self):
-        pass
 
-    def bool2indice(boolList):
-       return np.array([i for i, x in enumerate(boolList) if x])
+def bool2indice(boolList):
+    return np.array([i for i, x in enumerate(boolList) if x])
 
-    def indiceSeq2startEnd(indiceList):
-        indDiff = np.diff(indiceList)
-        starts = [indiceList[0]]
-        ends = list()
-        for i in range(1,len(indiceList)):
-            if indDiff[i] !=1:
-                ends.append(indiceList[i-1])
-                starts.append(indiceList[i])
-        ends.append(indiceList[i])
+def indiceSeq2startEnd(indiceList):
+    indDiff = np.diff(indiceList)
+    starts = [indiceList[0]]
+    ends = list()
+    for i in range(1,len(indiceList)):
+        if indDiff[i-1] !=1:
+            ends.append(indiceList[i-1])
+            starts.append(indiceList[i])
+    ends.append(indiceList[i])
+    return np.array(list(zip(starts,ends)))
+
+def boolSequence2startEndIndices(boolList):
+    indices = bool2indice(boolList)
+    return indiceSeq2startEnd(indices)
