92872b1c5f3da2fc96d879d299cddf8cb029f596
zerotonin <bgeurte@gwdg.de>
2020-07-29 11:11:10 +0200

    add bracket for start end indices

--- indexTools.py
+++ indexTools.py
@@ -4,13 +4,6 @@ import copy
 def bool2indice(boolList):
     return np.array([i for i, x in enumerate(boolList) if x])
 
-def bracketBools(boolList):
-    returnList = copy.deepcopy(boolList)
-    for i in range(1,len(returnList)-1):
-        if boolList[i] == 1:
-            returnList[i-1],returnList[i+1] = (True,True)
-    return returnList
-
 def indiceSeq2startEnd(indiceList):
     indDiff = np.diff(indiceList)
     starts = [indiceList[0]]
@@ -25,3 +18,17 @@ def indiceSeq2startEnd(indiceList):
 def boolSequence2startEndIndices(boolList):
     indices = bool2indice(boolList)
     return indiceSeq2startEnd(indices)
+
+def bracket_Bools(boolList):
+    returnList = copy.deepcopy(boolList)
+    for i in range(1,len(returnList)-1):
+        if boolList[i] == 1:
+            returnList[i-1],returnList[i+1] = (True,True)
+    return returnList
+
+def bracket_StartsEndOfSequence(startEndSequenceInd,seqLen):
+    for seqI in range(startEndSequenceInd.shape[0]):
+        
+        if startEndSequenceInd[seqI,0] != 0 and startEndSequenceInd[seqI,1] != seqLen:
+            startEndSequenceInd[seqI,0] = startEndSequenceInd[seqI,0]-1
+            startEndSequenceInd[seqI,1] = startEndSequenceInd[seqI,1]+1
