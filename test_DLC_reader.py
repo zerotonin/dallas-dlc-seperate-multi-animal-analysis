24ac4657fb3c7842f99f4cbdf92e84a551e737ef
zerotonin <bgeurte@gwdg.de>
2020-07-29 10:03:25 +0200

    update

--- test_DLC_reader.py
+++ test_DLC_reader.py
@@ -15,10 +15,6 @@ x.multiAnimal2numpy()
 y= DLC_reader.multiAnimalEval(x.tra)
 y.testForArtifacts()
 
-
-
-
-
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -28,7 +24,7 @@ ax.set_ylim(0,800)
 ax.axis('equal')
 plt.gca().invert_yaxis()
 
-for frameI in np.linspace(0,x.frameNo,500, endpoint=False, dtype=int ):
+for frameI in np.linspace(0,x.frameNo,150, endpoint=False, dtype=int ):
     
     for animalI in  range(x.animalNo):
         if y.artifactCandidates[frameI,animalI]:
@@ -41,22 +37,3 @@ for frameI in np.linspace(0,x.frameNo,500, endpoint=False, dtype=int ):
     fig.canvas.draw()
 
 
-tra3Sorted = np.array(y.posSorted) 
-tra3 = np.array(x.tra) 
-cmap = plt.get_cmap('tab20')      
-fig, axs = plt.subplots(2, 1)
-c = 0
-for frameI in tqdm.tqdm(range(tra3.shape[0])):
-    for animalI in range(tra3.shape[1]):
-        axs[0].plot(tra3[frameI,animalI,:,0],tra3[frameI,animalI,:,1],'o',color=cmap.colors[animalI])
-       # axs[1].plot(tra3Sorted[frameI,animalI,:,0],tra3Sorted[frameI,animalI,:,1],'o-',color=cmap.colors[animalI])
-        c+=1
-    
-
-
-axs[0].axis('equal')
-axs[1].axis('equal')
-
-plt.show()
-
-# plot single frame
