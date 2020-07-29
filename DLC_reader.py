96c217c3250dec4a506b8c38234dd3f858131879
zerotonin <bgeurte@gwdg.de>
2020-07-29 18:30:24 +0200

    swaped sorting for positional candidates | tralen -> frameNo

--- DLC_reader.py
+++ DLC_reader.py
@@ -34,7 +34,7 @@ class DLC_H5_reader:
 class multiAnimalEval:
     def __init__(self,tra3):
         self.tra = tra3
-        self.traLen,self.animalNo,self.bodyPartNo,self.coordNo = tra3.shape[:]
+        self.frameNo,self.animalNo,self.bodyPartNo,self.coordNo = tra3.shape[:]
         self.posSorted= list()
         self.accuracyThreshold = 0.95
 
@@ -43,8 +43,8 @@ class multiAnimalEval:
         return idx[0]  
 
     def calcBodyLen(self):
-        self.bodyLength = np.zeros(shape=(self.traLen,self.animalNo))
-        for frameI in range(self.traLen):
+        self.bodyLength = np.zeros(shape=(self.frameNo,self.animalNo))
+        for frameI in range(self.frameNo):
             for animalI in range(self.animalNo):
                 animal = self.tra[frameI,animalI,:,0:2]
                 self.bodyLength[frameI,animalI] = np.linalg.norm(np.diff(animal,axis=0)) 
@@ -56,8 +56,8 @@ class multiAnimalEval:
         #self.lengthDiff = np.abs(normBL -1)    
     
     def calcMaxStepSize(self):
-        self.step = np.zeros(shape=(self.traLen,self.animalNo))
-        for frameI in range(1,self.traLen):
+        self.step = np.zeros(shape=(self.frameNo,self.animalNo))
+        for frameI in range(1,self.frameNo):
             for animalI in range(self.animalNo):
                 tempSteps = []
                 for bodyPartI in range(self.bodyPartNo):
@@ -70,11 +70,12 @@ class multiAnimalEval:
         stepThreshold = np.percentile(self.step.flatten(),percentile)
         return self.step > stepThreshold
     
-    def testForArtifacts(self, stepThreshPerc = 99, bodyThresh= 2):
+    def testForArtifacts(self, stepThreshPerc = 99, bodyThresh= 2,sepCoord= 0):
         bodyLenCandidates  = self.testBodyLen(bodyThresh)
         stepSizeCandidates = self.testStepSize(stepThreshPerc)
+        positionCandidates = self.positionTest(sepCoord,sepCoordExt)
 
-        self.artifactCandidates = bodyLenCandidates | stepSizeCandidates
+        self.artifactCandidates = bodyLenCandidates | stepSizeCandidates | positionCandidates
 
  
 
@@ -85,25 +86,29 @@ class multiAnimalEval:
             if self.artifactCandidates[:,animalI].any() == True:
                 for bodyPartI in range(self.bodyPartNo):
                     z = trajectory_correcter.trajectory_corrector(self.tra[:,animalI,bodyPartI,0:2],self.artifactCandidates[:,animalI])
-                    z.interpolateOverArtifacts()
+                    z.interpolateOverArtifacts
                     self.tra[:,animalI,bodyPartI,0:2] = z.tra
 
      
 
-    def sortOnPos(self,seperaterCoord):
+    def positionTest(self,seperaterCoord):
+        allSepCoords = self.tra[:,:,:,seperaterCoord].flatten()
 
-        
+        boundaries = np.linspace(np.percentile(allSepCoords,1),np.percentile(allSepCoords,99) ,self.animalNo+1,endpoint=True)   
+        posCandidates = np.zeros(shape=(self.frameNo,self.animalNo),dtype=bool)   
+        for frameI in range(self.frameNo):
+            for animalI in range(self.animalNo):
+                sep_coords = self.tra[frameI,animalI,:,seperaterCoord]
+                for sep_coord in sep_coords:
+                    if ((boundaries[animalI] < sep_coord)  &  (boundaries[animalI+1] >= sep_coord)) == False:
+                        posCandidates[frameI,animalI] = True 
+        return posCandidates
+                    
 
-    
-    def Hungarian(ptsA,ptsB)
+
+    def Hungarian(ptsA,ptsB):
             C = cdist(ptsA,ptsB, 'euclidean')
             assignmentOld, assigmentNew = linear_sum_assignment(C)
             return assignmentOld, assigmentNew
-
-
-        
-
-
-            
 
 
