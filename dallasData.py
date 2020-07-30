6d5359cb3e82dd849abca038b2bebbef85bb9329
zerotonin <bgeurte@gwdg.de>
2020-07-30 18:22:32 +0200

    added dallas write out function

--- dallasData.py
+++ dallasData.py
@@ -1,5 +1,5 @@
 import numpy as np
-import json
+import json,os
 
 class dallasData():
     def __init__(self):
@@ -18,8 +18,43 @@ class dallasData():
         self.activity             = np.nan # percentage the animal was active during movie
         self.crossedMidLine       = False  # did the fly cross the midline -> classic Benzer 
         self.reachedTop           = False  # did the fly reach the 90% height line
+        self.pix2mmFactor         = np.nan # pixel 2 mm convertion factor
         self.movieFileName        = ''     # file location of the movie
         self.anaObjFileName       = ''     # file location of the pickle object 
         self.traCSVFileName       = ''     # file location of the flies tra file
         self.exampePictureFN      = ''     # file location of the example Picture
+    
+    def writeFly2JSON(self,fileName,directory):
+        # output filenames
+        csvFPos  = os.path.join(directory,fileName+'.csv')
+        jsonFPos = os.path.join(directory,fileName+'.json')
+        # write trajectory_away
+        numpy.savetxt(csvFPos, self.trajectory, delimiter=",")
+
+        # dict that shit
+        out_dict = {'movieFileName       ': self.movieFileName       ,
+                     'flyID               ': self.flyID               ,
+                     'frameNo             ': self.frameNo             ,
+                     'animalNo            ': self.animalNo            ,
+                     'coordNo             ': self.coordNo             ,
+                     'framesPerSecond     ': self.framesPerSecond     ,
+                     'speedSumABS         ': self.speedSumABS         ,
+                     'speedThrust         ': self.speedThrust         ,
+                     'speedSlip           ': self.speedSlip           ,
+                     'speedYaw            ': self.speedYaw            ,
+                     'predominantBodyAngle': self.predominantBodyAngle,
+                     'dropNo              ': self.dropNo              ,
+                     'activity            ': self.activity            ,
+                     'crossedMidLine      ': self.crossedMidLine      ,
+                     'reachedTop          ': self.reachedTop          ,
+                     'pix2mmFactor        ': self.pix2mmFactor        ,
+                     'movieFileName       ': self.movieFileName       ,
+                     'anaObjFileName      ': self.anaObjFileName      ,
+                     'traCSVFileName      ': self.traCSVFileName      ,
+                     'exampePictureFN     ': self.exampePictureFN     }
+
+        with open(jsonFPos, 'w') as outfile:
+            json.dump(data, outfile)
+
+        
     
