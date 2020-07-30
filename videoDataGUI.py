ca545b95fd45e70f176a8d63e076d4c618692129
zerotonin <bgeurte@gwdg.de>
2020-07-30 21:38:02 +0200

    interactive video corner extraction

---
+++ videoDataGUI.py
@@ -0,0 +1,55 @@
+import mediaHandler,time
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+class videoDataGUI():
+
+    def __init__(self,videoPos,vidType='movie'):
+        self.media     = mediaHandler.mediaHandler(videoPos,vidType)
+
+    
+    def tellme(self,s):
+        print(s)
+        plt.title(s, fontsize=16)
+        plt.draw()
+
+    
+    def getArenaData(self,frame):
+        imgplot = plt.imshow(frame)
+
+        plt.setp(plt.gca(), autoscale_on=False)
+
+        self.tellme('You will define a box, click to begin')
+
+        plt.waitforbuttonpress()
+
+        while True:
+            pts = []
+            while len(pts) < 4:
+                self.tellme('Select 4 corners with mouse')
+                pts = np.asarray(plt.ginput(4, timeout=-1))
+                if len(pts) < 4:
+                    self.tellme('Too few points, starting over')
+                    time.sleep(1)  # Wait a second
+
+            ph = plt.fill(pts[:, 0], pts[:, 1], 'y', lw=2,alpha=0.5)
+
+            self.tellme('Happy? Key click for yes, mouse click for no')
+
+            if plt.waitforbuttonpress():
+                break
+
+            # Get rid of fill
+            for p in ph:
+                p.remove()
+        #clear overlay
+        for p in ph:
+            p.remove()
+        return pts
+
+    def run(self):
+        self.frame = self.media.getFrame(int(self.media.length/2))
+        self.arenaCoordinates = self.getArenaData(self.frame)
+        return self.arenaCoordinates
