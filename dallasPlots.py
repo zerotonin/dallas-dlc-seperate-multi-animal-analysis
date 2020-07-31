db97a7273d0e54053d5970ab91887a199a84b082
zerotonin <bgeurte@gwdg.de>
2020-07-31 19:20:04 +0200

    changed to make lollipop plots easier

--- dallasPlots.py
+++ dallasPlots.py
@@ -2,26 +2,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def standardPlot(frame,traObj,traSteps):
+    plt.ion()
     fig = plt.figure()
     ax = fig.add_subplot(111)
     cmap = plt.get_cmap('tab20b')  
     ax.imshow(frame)
     plotSlots(traObj,ax)
-    plotTra(traObj,ax,cmap,traSteps)
+    plotTraPix(traObj,ax,cmap,traSteps)
     plt.show()
+
+def standardPlotTrajectory(tra,traSteps,animalColor):
+    plt.ion()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plotLollipop(tra,ax,animalColor,traSteps)
+    ax.axis('equal')
+    plt.show()
+
 
 def plotSlots(traObj,ax):
         for animalI in  range(traObj.animalNo):
             ax.fill(traObj.slotCoord[animalI][:,0],traObj.slotCoord[animalI][:,1],fill=None,edgecolor='salmon') 
 
-def plotTra(traObj,ax,cmap,traSteps):
-    for frameI in np.linspace(0,traObj.frameNo,traSteps, endpoint=False, dtype=int ):
+def plotTraPix(traObj,ax,cmap,traSteps):
 
-        for animalI in  range(traObj.animalNo):
+    for animalI in  range(traObj.animalNo):
+        plotLollipop(traObj.tra[:,animalI,:,0:2],ax,cmap.colors[animalI],traSteps)
             
-            ax.plot(traObj.tra[frameI,animalI,:,0],traObj.tra[frameI,animalI,:,1],'-',color=cmap.colors[animalI])        
-            ax.plot(traObj.tra[frameI,animalI,0,0],traObj.tra[frameI,animalI,0,1],'.',color=cmap.colors[animalI])
-
+            
+def plotLollipop(tra,ax,animalColor,traSteps):
+    for frameI in np.linspace(0,tra.shape[0],traSteps, endpoint=False, dtype=int ):
+        ax.plot(tra[frameI,:,0],tra[frameI,:,1],'-',color=animalColor)        
+        ax.plot(tra[frameI,0,0],tra[frameI,0,1],'.',color=animalColor)
 
 
 def plotErrorTra(traObj,ax,cmap):
@@ -29,7 +41,7 @@ def plotErrorTra(traObj,ax,cmap):
 
         for animalI in  range(traObj.animalNo):
 
-            if y.artifactCandidates[frameI,animalI]:
+            if traObj.artifactCandidates[frameI,animalI]:
                 ax.plot(traObj.tra[frameI,animalI,:,0],traObj.tra[frameI,animalI,:,1],'k-')        
                 ax.plot(traObj.tra[frameI,animalI,:,0],traObj.tra[frameI,animalI,:,1],'kx')
             else:
