80ed2bbabf120d987b308b1ce5c01ba55db314b9
zerotonin <bgeurte@gwdg.de>
2020-08-05 10:45:18 +0200

    transcoding complete directory

--- test_Media.py
+++ test_Media.py
@@ -1,31 +1,16 @@
-import mediaHandler,cv2,os,tqdm
+import mediaHandler,os,tqdm
 from importlib import reload 
-import matplotlib.pyplot as plt
 from pathlib import Path
 
+reload(mediaHandler)
 
-testFile = Path('/media/gwdg-backup/BackUp/Anka/2018-04-19__10_41_58.seq')
-# Define the codec and create VideoWriter object 
-fourcc = cv2.VideoWriter_fourcc('m','j','p','g')
-targetFile = os.path.join(testFile.parent,testFile.name[:-3] +'mp4'    )
-mhObj = mediaHandler.mediaHandler(testFile,'norpix')
-sourceFPS = round(mhObj.fps)
-frameShape = mhObj.media.frame_shape  
-frameShape = (frameShape[1],frameShape[0])   
-allocatedFrames = mhObj.media.header_dict['allocated_frames']    
+sourceDir ='/media/bgeurten/Anka/Anka2/19_05_17'
+seq_Files = [f for f in os.listdir(sourceDir) if f.endswith('.seq')]
+seq_Files.sort()
 
-out = cv2.VideoWriter(targetFile,fourcc, sourceFPS,frameShape) 
-
-for frameNo in tqdm.tqdm(range(1000)):#allocatedFrames)):
-#frameNo  = 23
-    frame = mhObj.getFrame(frameNo)
-    #gray = cv2.normalize(frame, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    gray_3c = cv2.merge([frame, frame, frame])
-    frame = cv2.flip(frame,0)
-    out.write(gray_3c)
-#cv2.imshow('frame',frame)
-
-frameNo  = 23
-frame = mhObj.getFrame(frameNo)
-imgplot = plt.imshow(frame)
-plt.show()
+for seqFile in tqdm.tqdm(seq_Files,desc='files seq->avi: '):
+    seqFile = os.path.join(sourceDir,seqFile)
+    targetFile = seqFile[:-3] +'avi'    
+    mhObj = mediaHandler.mediaHandler(seqFile,'norpix')
+    mhObj.transcode_seq2avis(targetFile)
+    os.system('clear')
