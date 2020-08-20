dc04a7b04d74f157c2b97c93e3c0fa58157de2c1
zerotonin <bgeurte@gwdg.de>
2020-08-20 12:06:47 +0200

    added image registration function

--- mediaHandler.py
+++ mediaHandler.py
@@ -120,3 +120,83 @@ class mediaHandler():
             out.release()
         else:
             print('This function only works with norpix movie files')
+    
+    def register_movie(self, targetFile):
+        
+        # Get information about the norpix file
+        sourceFPS = round(self.fps)
+        frameShape = self.media.frame_shape  
+        frameShape = (frameShape[1],frameShape[0]) 
+        allocatedFrames = self.media.header_dict['allocated_frames']  
+
+        # Define the codec and create VideoWriter object 
+        fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+        out = cv2.VideoWriter(targetFile,fourcc, sourceFPS,frameShape)   
+
+        # we take the central frame of the movie as a registration template
+        template = self.getFrame(round(allocatedFrames/2))
+
+        for frameNo in tqdm.tqdm(range(allocatedFrames),desc='trasconding '+self.fileName):
+            frame = self.getFrame(frameNo)
+            regFrame = registerImage(template,frame)
+            out.write(regFrame)
+
+        out.release()
+
+    def registerImage(template,image2match):
+        '''
+            Image registration based on opbject detection in open cv
+            Taken and adapted from: 
+            https://www.geeksforgeeks.org/image-registration-using-opencv-python/
+        '''
+
+        # Open the image files. 
+        image2match = cv2.imread("im1.jpg") # Image to be aligned. 
+        template = cv2.imread("im22.jpg") # Reference image. 
+
+        # Convert to grayscale. 
+        img1 = cv2.cvtColor(image2match, cv2.COLOR_BGR2GRAY) 
+        img2 = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY) 
+        height, width = img2.shape 
+
+        # Create ORB detector with 5000 features. 
+        orb_detector = cv2.ORB_create(5000) 
+
+        # Find keypoints and descriptors. 
+        # The first arg is the image, second arg is the mask 
+        # (which is not reqiured in this case). 
+        kp1, d1 = orb_detector.detectAndCompute(img1, None) 
+        kp2, d2 = orb_detector.detectAndCompute(img2, None) 
+
+        # Match features between the two images. 
+        # We create a Brute Force matcher with 
+        # Hamming distance as measurement mode. 
+        matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True) 
+
+        # Match the two sets of descriptors. 
+        matches = matcher.match(d1, d2) 
+
+        # Sort matches on the basis of their Hamming distance. 
+        matches.sort(key = lambda x: x.distance) 
+
+        # Take the top 90 % matches forward. 
+        matches = matches[:int(len(matches)*90)] 
+        no_of_matches = len(matches) 
+
+        # Define empty matrices of shape no_of_matches * 2. 
+        p1 = np.zeros((no_of_matches, 2)) 
+        p2 = np.zeros((no_of_matches, 2)) 
+
+        for i in range(len(matches)): 
+            p1[i, :] = kp1[matches[i].queryIdx].pt 
+            p2[i, :] = kp2[matches[i].trainIdx].pt 
+
+        # Find the homography matrix. 
+        homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC) 
+
+        # Use this matrix to transform the 
+        # colored image wrt the reference image. 
+        transformed_img = cv2.warpPerspective(image2match, homography, (width, height)) 
+
+        # return the matched img
+        return transformed_img
