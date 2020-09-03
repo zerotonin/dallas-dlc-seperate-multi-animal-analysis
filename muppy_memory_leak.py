275d2bb3ed27c5fc6f83b97f2390829cc22be547
zerotonin <bgeurte@gwdg.de>
2020-09-03 12:55:46 +0200

    added function

--- muppy_memory_leak.py
+++ muppy_memory_leak.py
@@ -1,13 +1,14 @@
 from pympler import muppy, summary
 import pandas as pd
 
-all_objects = muppy.get_objects()
-sum1 = summary.summarize(all_objects)# Prints out a summary of the large objects
+def checkMemory():
+    all_objects = muppy.get_objects()
+    sum1 = summary.summarize(all_objects)# Prints out a summary of the large objects
 
-summary.print_(sum1)# Get references to certain types of objects such as dataframe
+    summary.print_(sum1)# Get references to certain types of objects such as dataframe
 
-dataframes = [ao for ao in all_objects if isinstance(ao, pd.DataFrame)]for d in dataframes:
+    dataframes = [ao for ao in all_objects if isinstance(ao, pd.DataFrame)]
 
-print(d.columns.values)
-
-print len(d)
+    for d in dataframes:
+        print(d.columns.values)
+        print(len(d))
