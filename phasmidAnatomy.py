ac5a78941953f097614f63b77d1d580799c5ad15
Bart Geurten <bgeurte@gwdg.de>
2020-09-21 08:03:40 +0200

    init cannot return

--- phasmidAnatomy.py
+++ phasmidAnatomy.py
@@ -3,10 +3,10 @@ class phasmidAnatomy:
 
         self.knownSpecies = ['Sungaya inexpectata','Medauroidea extradentata','Neohirasea maerens']
         
-        workFlag = True
+        readyFlag = True
         if gender!='f' and gender!='m':
             print('Unknown gender: ' + gender)
-            workFlag = False
+            readyFlag = False
         else:
             self.gender = gender
 
@@ -15,8 +15,8 @@ class phasmidAnatomy:
             self.getPhasmidAnatomy()
         else:
             print('Unknown species: ' +species)
-            workFlag = False
-        return workFlag
+            readyFlag = False
+        self.ready = readyFlag
         
     def getAxisLength(self,axisName):
         if axisName in self.anatomy.keys():
