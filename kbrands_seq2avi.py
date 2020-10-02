307803295023f6083c8c0dafade377d8079ace62
Kathy Brands <katharina.brands@stud.uni-goettingen.de>
2020-10-02 14:45:23 +0200

    new run time for K.brands videos

---
+++ kbrands_seq2avi.py
@@ -0,0 +1,43 @@
+import mediaHandler,os,tqdm,copy,shutil
+import numpy as np
+from pathlib import Path
+from importlib import reload 
+from cv2 import cv2
+
+def transcodeSeqLocally(seqFile,sourceDir,localDir,expectedSize = 0):
+    
+    sourcePos = os.path.join(sourceDir,seqFile)
+    localPos  = os.path.join(localDir,seqFile)
+    targetPos = sourcePos[:-3] +'avi'    
+    
+    print('source position: ',sourcePos)    
+    print('local position: ',localPos)
+    print('target position: ',targetPos)
+    if os.path.getsize(sourcePos) >= expectedSize:
+
+        try:
+            shutil.move(sourcePos,localPos)
+            mhObj = mediaHandler.mediaHandler(localPos,'norpix')
+            mhObj.transcode_seq2avis(targetPos)
+            os.remove(localPos)
+        except:
+            print('Could not handle: ', sourcePos)
+    else:
+        print('File size too small: ', sourcePos) 
+    os.system('clear')
+
+##############
+# USER INPUT #
+##############
+
+sourceDir = '/media/gwdg-backup/BackUp/KathyBrands/MasterVideos'
+localDir  = '/home/kbrands/Videos'
+expectedSize = 0 #80000000000 about 80GB
+
+# run time
+seq_Files = [f for f in os.listdir(sourceDir) if f.endswith('.seq')]
+seq_Files.sort()
+os.system('clear')
+
+for seqFile in tqdm.tqdm(seq_Files,desc='files seq->avi: '):
+    transcodeSeqLocally(seqFile,sourceDir,localDir,expectedSize)
