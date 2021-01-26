08ad71b44d0c81fed31c90e5f4fd6607f6e08e10
Bart Geurten <bgeurte@gwdg.de>
2021-01-26 09:44:53 +0100

    reading frameNumber and imageObjects

--- charonFoodTra.py
+++ charonFoodTra.py
@@ -1,6 +1,22 @@
-def readFoodTra(fileName):
+def readFoodRecResult(fileName):
     file1 = open(fileName, 'r') 
     lines = file1.readlines() 
     return lines
-    
-data = readFoodTra('foodTestTra.tra')          
+
+def splitLineIntoImageObjects(line):
+    objectList = line.split('>')
+    del(objectList[0]) # first entry is frameNumber
+    return objectList
+
+def getFrameNumberFromLine(line):
+    objectList = line.split('>')
+    frameNumber = int(objectList[0][0:-3])
+    return frameNumber
+
+def readImageObject(imageObjectString):
+    pass
+
+
+data = readFoodRecResult('foodTestTra.tra') 
+imageObjects = splitLineIntoImageObjects(data[5])  
+frameNumber  = getFrameNumberFromLine(data[5])       
