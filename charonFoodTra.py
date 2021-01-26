3c3929f6b7e7b5ad1dcbe62d73e2fd4ffc5a087a
Bart Geurten <bgeurte@gwdg.de>
2021-01-26 10:32:36 +0100

    made to class

--- charonFoodTra.py
+++ charonFoodTra.py
@@ -1,44 +1,57 @@
-def readFoodRecResult(fileName):
-    file1 = open(fileName, 'r') 
-    lines = file1.readlines() 
-    return lines
+class readCharonFood54():
+    def __init__(self,filePosition):
 
-def splitLineIntoImageObjects(line):
-    objectList = line.split('>')
-    del(objectList[0]) # first entry is frameNumber
-    return objectList
+        self.filePosition = filePosition
+        self.rawTextData  = []
 
-def getFrameNumberFromLine(line):
-    objectList = line.split('>')
-    frameNumber = int(objectList[0][0:-3])
-    return frameNumber
-
-def readImageObject(imageObjectString):
-    objectValList=imageObjectString.split(',')
-    objectValList[5]= objectValList[5][0:-2]
-    for i in range(1,6):
-        objectValList[i]= float(objectValList[i])
-
-    return objectValList
-
-def imObjectVal2imObjDict(objectValList):
-    imObjName = objectValList[0]
-    quality   = objectValList[1]
-    boundingBoxCoordinates = tuple(objectValList[2::])   
-    x,y = boundingBox2centerOfMass(boundingBoxCoordinates)
-    return {'name':imObjName,'centerOfMass': (x,y), 'quality': quality, 'boundingBox': boundingBoxCoordinates}
-
-def boundingBox2centerOfMass(boundingBoxCoordinates):
-    x = (boundingBoxCoordinates[0]+boundingBoxCoordinates[2])/2.0
-    y = (boundingBoxCoordinates[1]+boundingBoxCoordinates[3])/2.0
-    return x,y
+    def readFoodRecResult(self):
+        file1 = open(self.filePosition, 'r') 
+        self.rawTextData = file1.readlines() 
 
 
+    def splitLineIntoImageObjects(self,line):
+        objectList = line.split('>')
+        del(objectList[0]) # first entry is frameNumber
+        return objectList
 
-data = readFoodRecResult('foodTestTra.tra') 
-imageObjects  = splitLineIntoImageObjects(data[5])  
-frameNumber   = getFrameNumberFromLine(data[5])
+    def getFrameNumberFromLine(self,line):
+        objectList = line.split('>')
+        frameNumber = int(objectList[0][0:-3])
+        return frameNumber
 
-for imObj in imageObjects:
-    objectValList = readImageObject(imObj)
-    imObjDict  = imObjectVal2imObjDict(objectValList)       
+    def readImageObject(self,imageObjectString):
+        objectValList=imageObjectString.split(',')
+        objectValList[5]= objectValList[5][0:-2]
+        for i in range(1,6):
+            objectValList[i]= float(objectValList[i])
+
+        return objectValList
+
+    def imObjectVal2imObjDict(self,objectValList):
+        imObjName = objectValList[0]
+        quality   = objectValList[1]
+        boundingBoxCoordinates = tuple(objectValList[2::])   
+        x,y = self.boundingBox2centerOfMass(boundingBoxCoordinates)
+        return {'name':imObjName,'centerOfMass': (x,y), 'quality': quality, 'boundingBox': boundingBoxCoordinates}
+
+    def boundingBox2centerOfMass(self,boundingBoxCoordinates):
+        x = (boundingBoxCoordinates[0]+boundingBoxCoordinates[2])/2.0
+        y = (boundingBoxCoordinates[1]+boundingBoxCoordinates[3])/2.0
+        return x,y
+    
+    def readImObjPerFrame(self,line): 
+        imageObjects  = self.splitLineIntoImageObjects(line)  
+        frameNumber   = self.getFrameNumberFromLine(line)
+        imObjList = [frameNumber]
+        for imObj in imageObjects:
+            objectValList = self.readImageObject(imObj)
+            imObjDict     = self.imObjectVal2imObjDict(objectValList)
+            imObjList.append(imObjDict)       
+        return imObjList
+
+
+
+    
+paul= readCharonFood54('foodTestTra.tra') 
+paul.readFoodRecResult()
+imObjLineList = paul.readImObjPerFrame(paul.rawTextData[2])
