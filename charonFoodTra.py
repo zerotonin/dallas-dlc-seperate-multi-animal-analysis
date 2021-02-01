f247685ca160d922b6e47f3d05633484f6af9a47
Katharina Brands <katharina.brands@stud.uni-goettingen.de>
2021-02-01 22:17:17 +0100

    assignFly2Arena

--- charonFoodTra.py
+++ charonFoodTra.py
@@ -1,17 +1,17 @@
 class readCharonFood54():
     def __init__(self,filePosition):
 
-        self.filePosition = filePosition
-        self.rawTextData  = []
+        self.filePosition = filePosition #filePosition = file name
+        self.rawTextData  = []       # woher kommt rawTextData? muss es nihct als attribut in der funktion (def init) angegeben werden?
         #self.readFile()
 
     def readFoodRecResult(self):
-        file1 = open(self.filePosition, 'r') 
-        self.rawTextData = file1.readlines() 
+        file1 = open(self.filePosition, 'r')  # 'r': open for reading
+        self.rawTextData = file1.readlines()  # reading row wise 
+        print(self.rawTextData)               # and print it into the rawTextData List?
 
-
-    def splitLineIntoImageObjects(self,line):
-        objectList = line.split('>')
+    def splitLineIntoImageObjects(self,line): 
+        objectList = line.split('>')  # each object is encapsuilated in ><
         del(objectList[0]) # first entry is frameNumber
         return objectList
 
@@ -21,19 +21,19 @@ class readCharonFood54():
         return frameNumber
 
     def readImageObject(self,imageObjectString):
-        objectValList=imageObjectString.split(',')
-        objectValList[5]= objectValList[5][0:-2]
+        objectValList=imageObjectString.split(',') # object Values are seperated by ','
+        objectValList[5]= objectValList[5][0:-2] # 6 object Values (0-5)
         for i in range(1,6):
-            objectValList[i]= float(objectValList[i])
+            objectValList[i]= float(objectValList[i]) # convert string into floating point number
 
         return objectValList
 
-    def imObjectVal2imObjDict(self,objectValList):
+    def imObjectVal2imObjDict(self,objectValList): # putting objectValues into Dictionary
         imObjName = objectValList[0]
         quality   = objectValList[1]
-        boundingBoxCoordinates = tuple(objectValList[2::])   
+        boundingBoxCoordinates = tuple(objectValList[2::])   # a Tupel is a finite ordered list of elements
         x,y = self.boundingBox2centerOfMass(boundingBoxCoordinates)
-        return {'name':imObjName,'centerOfMass': (x,y), 'quality': quality, 'boundingBox': boundingBoxCoordinates}
+        return {'name':imObjName,'centerOfMass': (x,y), 'quality': quality, 'boundingBox': boundingBoxCoordinates} #dictionary{key:value pairs}
 
     def boundingBox2centerOfMass(self,boundingBoxCoordinates):
         x = (boundingBoxCoordinates[0]+boundingBoxCoordinates[2])/2.0
@@ -58,6 +58,27 @@ class readCharonFood54():
     def readFile(self):
         self.readFoodRecResult()
         self.convertRecordingtoListDict()
+
+    def assingFly2Arena(self):
+        #imObjectVal2imObjDict.get('name')
+        imObjName = objectValList[0]
+        flyId=0
+        for flyId in imObjectVal2imObjDict:
+            if imObjName == 'fly':
+                flyPosition = imObjectVal2imObjDict.get('centerOfMass')
+            flyId += 1
+            return flyPosition
+            
+        ArenaId=0
+        for ArenaId in imObjectVal2imObjDict:
+            if imObjName == 'Arena':
+                ArenaPosition = imObjectVal2imObjDict.get('boundingBoxCoordinates')
+            ArenaId += 1
+            return ArenaPosition
+        
+        if flyPosition >= boundingBoxCoordinates[0,1] and flyPosition <= boundingBoxCoordinates[2,3]:
+            return flyId, ArenaId 
     
+
 paul= readCharonFood54('foodTestTra.tra') 
 paul.readFile()
