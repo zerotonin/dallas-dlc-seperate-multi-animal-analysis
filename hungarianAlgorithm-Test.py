782d68b8303dd282442888fc4cc4a4383bd9e188
Katharina Brands <katharina.brands@stud.uni-goettingen.de>
2021-02-22 10:12:27 +0100

    list of distances

--- hungarianAlgorithm-Test.py
+++ hungarianAlgorithm-Test.py
@@ -1,5 +1,5 @@
 # vectorNorm = getVectorNorm(arenaList1[0],arenaList2[2])
-# distMat = getAdjacencyMatrix(arenaList1,arenaList2)
+
 '''
 def getVectorNorm (arenaList):
     VN = list()
@@ -18,22 +18,24 @@ def getVectorNorm (arenaList):
 
 def VN_multipleFrames (sortedArenaList1, arenaList2):
     VN_List = list()
-    Frame1ArenaC = 0
+    Frame1ArenaC = 1
     for arena in sortedArenaList1:
         CMx1 = arena['centerOfMass'][0]
         CMy1 = arena['centerOfMass'][1]
 
-        Frame2ArenaC = 0
+        Frame2ArenaC = 1
         for arena in arenaList2:
             CMx2 = arena['centerOfMass'][0]
             CMy2 = arena['centerOfMass'][1]
             vectorNorm = np.sqrt(np.square(CMx1-CMx2)+np.square(CMy1-CMy2))
+            # returns a List of Distances between Arenas in Frame 1 and each Arena in Frame 2
+            VN_List.append('The Distance between the CM of Arena ' + str(Frame1ArenaC) + ' in Frame 1 and the CM of Arena ' + str(Frame2ArenaC) + ' in Frame 2 is: ' + str(vectorNorm))
             Frame2ArenaC += 1
-            VN_List.append('The Distance between the CM of Arena ' + str(Frame1ArenaC) + ' in Frame 1 and the CM of Arena ' + str(Frame2ArenaC) + ' is: ' + str(vectorNorm))
         Frame1ArenaC += 1
     return (VN_List)
 
-    
+# distMat = getAdjacencyMatrix(arenaList1,arenaList2)
+   
 
 #VN_ArenaList1 = getVectorNorm(arenaList1)
 #VN_ArenaList1 = getVectorNorm(arenaList2)
