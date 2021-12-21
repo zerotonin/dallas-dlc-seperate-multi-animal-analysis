51f0fa3f866b322045a31ce3e54d09ea35e77d95
Bart Geurten <bgeurte@gwdg.de>
2021-12-21 23:55:43 +0100

    more testing

--- analyseYegiData.py
+++ analyseYegiData.py
@@ -4,20 +4,27 @@ import numpy as np
 from tqdm import tqdm
 import pandas as pd
 import cv2 as cv
+import matplotlib.pyplot as plt
 
-sourceDir = '/media/gwdg-backup/BackUp/Yegi'
-traList = [os.path.join(sourceDir,x) for x in os.listdir(sourceDir) if x.endswith('.tra')]
-traList = [x for x in traList if  os.path.isfile(x[0:-3]+'csv')]
-traList.sort()
-bbList  = [x[0:-3]+'csv' for x in traList]
-movList = [x[0:-3]+'avi' for x in traList]
 
-traFile = traList[0]
-bbFile  = bbList[0]
-movFile = movList[0]
 # read and transform bounding box of arena position
 
-cmar = readMultiAnimalCharonTra.readMultiAnimalCharonTra(traFile,movFile,bbFile)
-cmar.run()
-cmar.saveAna('/media/dataSSD/YegiTra/arena.csv','/media/dataSSD/YegiTra/tra.h5')
+#cmar = readMultiAnimalCharonTra.readMultiAnimalCharonTra(traFile,movFile,bbFile,20.0,20.0)
+#cmar.run()
+#cmar.saveAna('/media/dataSSD/YegiTra/arena.csv','/media/dataSSD/YegiTra/tra.h5')
 
+
+fps = 10
+df = pd.read_hdf('/media/dataSSD/YegiTra/tra.h5','data')
+for arenaNo in range(20):
+    subset = df.loc[df['arenaNo']==arenaNo,:].copy()
+    subset['coord_mmArena_y'] = subset['coord_mmArena_y'].interpolate()
+    subset['coord_mmArena_x'] = subset['coord_mmArena_x'].interpolate()
+    subset['coord_rho'] = subset['coord_rho'].interpolate()
+    subset['coord_phi'] = subset['coord_phi'].interpolate()
+    # need to low pass filter first!
+    subset['absSpeed_mm/s'] = np.insert(np.sqrt(np.diff(subset['coord_mmArena_y'].values)**2+np.diff(subset['coord_mmArena_x'].values)**2)*fps,0,np.NaN)
+
+plt.plot(subset['coord_mmArena_x'],subset['coord_mmArena_y'])     
+plt.axis('equal')
+plt.show()
