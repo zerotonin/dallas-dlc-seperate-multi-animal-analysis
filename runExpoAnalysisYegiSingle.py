727b9ba8da98b8ef5a6107b62ba0ff4cce691aff
Bart Geurten <bgeurte@gwdg.de>
2022-03-02 14:33:49 +0100

    single version of taspExpoAnalysisYegi

---
+++ runExpoAnalysisYegiSingle.py
@@ -0,0 +1,33 @@
+import readMultiAnimalCharonTra
+import explorationRateBasedonBB
+import augmentYegiTra
+import os
+
+
+traFilePos              =  '/media/gwdg-backup/BackUp/Yegi/2020-11-24__14_15_16.tra' 
+fps                     = 10 
+activityThreshold       = 0.3
+arenaBoundingBoxFilePos =  '/media/gwdg-backup/BackUp/Yegi/2020-11-24__14_15_16.csv'
+movieFilePos            =  '/media/gwdg-backup/BackUp/Yegi/2020-11-24__14_15_16.avi'
+arenaWidthMM            = 20.0 
+arenaHeightMM           = 20.0 
+outputDir               = '/media/dataSSD/YegiTra'
+metaDataFlag            = '<29'
+fileName     = os.path.basename(traFilePos)[0:-4]   
+cleanTraOut  = os.path.join(outputDir,fileName+'_trajectory.h5')
+meanArenaOut = os.path.join(outputDir,fileName+'_meanArena.csv')
+
+# read trajectory
+cmar = readMultiAnimalCharonTra.readMultiAnimalCharonTra(traFilePos,movieFilePos,arenaBoundingBoxFilePos,arenaHeightMM,arenaWidthMM)
+cmar.run()
+cmar.saveAna(meanArenaOut,cleanTraOut)
+del(cmar)
+#augement data
+ayt = augmentYegiTra.augmentYegiTra(cleanTraOut,metaDataFlag,fps,activityThreshold)
+ayt.run()
+
+#calculate exploration rate
+expo = explorationRateBasedonBB.explorationRateCalculator(ayt.savePos)
+del(ayt)
+expo.analyseAllAnimals()
+
